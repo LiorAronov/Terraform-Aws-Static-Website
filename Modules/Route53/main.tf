4946c2df69303c6182dc7e239f34371d57012282
LiorAronov <lioraronov@gmail.com>
2023-03-09 20:19:02 +0200

    Add alias records to domain bucket.

---
+++ Modules/Route53/main.tf
@@ -0,0 +1,13 @@
+##########################################################################
+# Add alias records to domain bucket.
+##########################################################################
+resource "aws_route53_record" "domain_bucket" {
+  zone_id = data.aws_route53_zone.hosted_zones_1.id
+  name    = var.pass_variables_domain_name
+  type    = "A"
+  alias {
+    name = data.aws_s3_bucket.domain_bucket_data.website_domain
+    zone_id = data.aws_s3_bucket.domain_bucket_data.hosted_zone_id
+    evaluate_target_health = true 
+  }
+}
