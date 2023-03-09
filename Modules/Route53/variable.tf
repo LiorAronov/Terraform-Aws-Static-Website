13daaa87f956b9ad51f122ffbd78fc8c7093af08
LiorAronov <lioraronov@gmail.com>
2023-03-09 20:18:39 +0200

    The variable of the Route53 Modules.

---
+++ Modules/Route53/variable.tf
@@ -0,0 +1,6 @@
+##########################################################################
+# The variable of the Remote-State-Locking-State Modules.
+##########################################################################
+variable "pass_variables_domain_name" {
+    type = string
+}
