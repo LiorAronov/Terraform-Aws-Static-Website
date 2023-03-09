d65025eb6b2a371379f992d87bfa34f5082dcde9
LiorAronov <lioraronov@gmail.com>
2023-03-09 20:19:23 +0200

    Add alias records to sub-domain bucket.

--- Modules/Route53/main.tf
+++ Modules/Route53/main.tf
@@ -11,3 +11,17 @@ resource "aws_route53_record" "domain_bucket" {
     evaluate_target_health = true 
   }
 }
+
+##########################################################################
+# Add alias records to sub-domain bucket.
+##########################################################################
+resource "aws_route53_record" "subdomain_bucket" {
+  zone_id = data.aws_route53_zone.hosted_zones_1.id
+  name    = "www.${var.pass_variables_domain_name}"
+  type    = "A"
+  alias {
+    name = data.aws_s3_bucket.subdomain_bucket_data.website_domain
+    zone_id = data.aws_s3_bucket.subdomain_bucket_data.hosted_zone_id
+    evaluate_target_health = true  
+  }
+}
