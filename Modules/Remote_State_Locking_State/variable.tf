eac5c09053257c5df1a7db7ec9ac78bec08d6e3e
LiorAronov <lioraronov@gmail.com>
2023-03-09 20:10:50 +0200

    The variable of the Remote-State-Locking-State Modules.

---
+++ Modules/Remote_State_Locking_State/variable.tf
@@ -0,0 +1,10 @@
+##########################################################################
+# The variable of the Remote-State-Locking-State Modules.
+##########################################################################
+variable "state_bucket_name" {
+    type = string
+}
+
+variable "state_locking_name" {
+    type = string
+}
