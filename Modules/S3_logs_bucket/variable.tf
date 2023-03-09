45ed3131d090ee385f1771437c13cbd4b8b637a5
LiorAronov <lioraronov@gmail.com>
2023-03-09 20:17:18 +0200

    The variable of the S3-logs-bucket Modules.

---
+++ Modules/S3_logs_bucket/variable.tf
@@ -0,0 +1,12 @@
+##########################################################################
+# The variable of the Remote-State-Locking-State Modules.
+##########################################################################
+variable "pass_variables_domain_name" {
+    type = string
+}
+##########################################################################
+# The variable of the Remote-State-Locking-State Modules.
+##########################################################################
+variable "logs_bucket_name" {
+    type = string
+}
