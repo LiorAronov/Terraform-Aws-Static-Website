8f74748e53555805d25281836029b43fcec30f24
LiorAronov <lioraronov@gmail.com>
2023-03-09 20:09:49 +0200

    The dynamodb table of the state locking.

--- Modules/Remote_State_Locking_State/main.tf
+++ Modules/Remote_State_Locking_State/main.tf
@@ -14,3 +14,17 @@ resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
     status = "Enabled"
     }
 }
+
+##########################################################################
+# The dynamodb table of the State Locking.
+##########################################################################
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = var.state_locking_name
+    # name = "terraform-state-locking-website-23-022023"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
