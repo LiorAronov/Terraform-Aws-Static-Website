4affa07280b2cd4bcdf5cd7916d5afb29c7e9bae
LiorAronov <lioraronov@gmail.com>
2023-03-09 20:21:29 +0200

    The variable of Dev folder.

---
+++ Dev/variable.tf
@@ -0,0 +1,15 @@
+###############################################################
+# The variable of Dev folder.
+###############################################################
+variable "domain_name" {
+  type        = string
+}
+variable "region" {
+  type        = string
+}
+variable "access_key" {
+  type        = string
+}
+variable "secret_key" {
+  type        = string
+}
